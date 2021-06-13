76e9f84d2c66ba3ba1341202d08232442b3d6ca9
ylber-gashi <58399076+ylber-gashi@users.noreply.github.com>
2021-06-13 15:26:44 +0200

    -Removed uncessary files

--- Hotel.Api.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hotel.Api.Domain
-{
-    public class Class1
-    {
-    }
-}
--- Hotel.Api.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hotel.Api.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
