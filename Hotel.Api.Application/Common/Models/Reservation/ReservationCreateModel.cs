0352eb889fcf2349d60937d9f0101e70e0216c34
xhanan <xhenan.shehu@studentet.uni-pr.edu>
2021-06-17 00:59:28 +0200

    Reservation Service Added

--- Hotel.Api.Application/Common/Models/Reservation/ReservationCreateModel.cs
+++ Hotel.Api.Application/Common/Models/Reservation/ReservationCreateModel.cs
@@ -1,6 +1,13 @@
-﻿namespace Hotel.Api.Application.Common.Models.Reservation
+﻿using System;
+
+namespace Hotel.Api.Application.Common.Models.Reservation
 {
     public class ReservationCreateModel
     {
+        public string UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public int PaymentId { get; set; }
     }
 }
--- Hotel.Api.Application/Common/Models/Reservation/ReservationUpdateModel.cs
+++ Hotel.Api.Application/Common/Models/Reservation/ReservationUpdateModel.cs
@@ -1,6 +1,12 @@
-﻿namespace Hotel.Api.Application.Common.Models.Reservation
+﻿using System;
+
+namespace Hotel.Api.Application.Common.Models.Reservation
 {
     public class ReservationUpdateModel
     {
+        public string UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
     }
 }
